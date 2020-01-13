3726c5b476a9c3923e6408e10b497e9a71184c5a
Jonas Jerndin <Jonas.Jerndin.ext@toyota-industries.eu>
2020-01-13 12:59:19 +0100

    Add test for using Mailhog as mail server

---
+++ docker-sayhello/src/SayHelloApp/Controllers/MailController.cs
@@ -0,0 +1,47 @@
+﻿using System.Net.Mail;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SayHelloApp.Controllers
+{
+    public class MailController : Controller
+    {
+        // GET: Mail
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Mail/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Mail/Create
+        [HttpPost]
+        // [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                using (var message = new MailMessage(from: "sender@example.com", to: "someone@else.com", subject: "Subject", body: "The body")
+                {
+                    IsBodyHtml = false
+                })
+                {
+                    using (var mailserver = new SmtpClient("mailserver", 1025))
+                    {
+
+                        mailserver.Send(message);
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
